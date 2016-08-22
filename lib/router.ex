30609d2c0486f3c3eede5fcc28d73b10c9301e8b
Henrik Fricke <henrik.fricke@jimdo.com>
2016-08-23 00:06:59 +0200

    add validation for facebook

--- lib/router.ex
+++ lib/router.ex
@@ -13,6 +13,16 @@ defmodule Justafacebookbot.Router do
     send_resp(conn, 200, "This entire website runs on Elixir plugs!...")
   end
 
+  get "/webhook" do
+    conn = Plug.Conn.fetch_query_params(conn)
+    if conn.params["hub.mode"] === "subscribe" &&
+      conn.params["hub.verify_token"] === System.get_env("VALIDATION") do
+        send_resp(conn, 200, conn.params["hub.challenge"])
+    else
+      send_resp(conn, 403, "Something went wrong.")
+    end
+  end
+
   match _ do
     send_resp(conn, 404, "oops")
   end
